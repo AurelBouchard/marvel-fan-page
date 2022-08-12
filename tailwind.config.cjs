63a1661b099a0f255f9b06184d7030c5322e92ae
butch <au.bouchard@gmail.com>
2022-08-12 11:33:20 +0200

    graphic charter : colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        textColor: 'rgb(198, 211, 231)', //'rgb(146, 171, 207)'
+        teal:{
+          DEFAULT: 'rgb(17, 236, 229)',
+          dark: 'rgba(17, 236, 229, 0.6)'
+        },
+        dark:{
+          DEFAULT: 'rgb(15, 23 ,36)',
+          darkest: 'rgb(6, 9, 15)'
+        },
+        lime: 'rgb(95, 239, 124)'
+      }
+    },
   },
   plugins: [],
 }
