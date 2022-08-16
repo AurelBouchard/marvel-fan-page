7da11fd7e5b30006007ef5dcc21c5915aa02b77a
butch <au.bouchard@gmail.com>
2022-08-16 09:02:22 +0200

    https endpoint

--- src/credentials.js
+++ src/credentials.js
@@ -1,7 +1,7 @@
 // MARVEL API
 
 // end point URL
-const url = "http://gateway.marvel.com/v1/public/"
+const url = "https://gateway.marvel.com/v1/public/"
 
 // public key
 const pubKey = "074682df827cdde830e4ed0cdfa00ba2"
