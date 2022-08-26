ca1eecf9c4cb5bea4c7a456f2d3aabe3ea3ba3bf
butch <au.bouchard@gmail.com>
2022-08-26 12:25:08 +0200

    loader bug fix

--- src/components/modals/Loader.jsx
+++ src/components/modals/Loader.jsx
@@ -1,6 +1,8 @@
 import React, {useEffect, useState} from 'react';
 import {api} from "../../credentials";
 import axios from "axios";
+import lowBandWidthLoader from '../../assets/short_comp.webp';
+import highBandWidthLoader from '../../assets/full_no_loss.webp'
 
 
 
@@ -171,8 +173,8 @@ export default function Loader({categories, setReady, ready, setNVAM, NVAM, setD
     
     return (
         <div className={`h-screen w-screen fixed top-0 left-0 md:flex justify-center items-center animate-appear-slow z-100`}>
-            <img src='../../src/assets/short_comp.webp' alt="" className={`flex-1 md:hidden`}/>
-            <img src='../../src/assets/full_no_loss.webp' alt="" className={`flex-1 hidden md:block`}/>
+            <img src={lowBandWidthLoader} alt="Marvel Fan Page loading" className={`flex-1 md:hidden`}/>
+            <img src={highBandWidthLoader} alt="Marvel Fan Page loading" className={`flex-1 hidden md:block`}/>
         </div>
     )
 }
