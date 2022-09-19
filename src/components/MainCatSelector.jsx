510ec6119b66ab895ba338eca305d0ee0d1cc54c
butch <au.bouchard@gmail.com>
2022-09-19 07:56:08 +0200

    cat index bug fix

--- src/components/MainCatSelector.jsx
+++ src/components/MainCatSelector.jsx
@@ -51,8 +51,8 @@ export default function MainCatSelector({expandable}) {
                     return (
                         <div key={index} className={`h-8 flex items-center hover:bg-teal-light hover:text-dark-darkest px-3 cursor-pointer`}
                              onClick={()=>{
-                                 console.log("mainCatSelector setCatIndex", index)
-                                 setItem(item => ({item, ...{catIndex: index}}))
+                                 //console.log("mainCatSelector setCatIndex", index)
+                                 setItem(item => ({...item, ...{catIndex: index}}))
                              }}
                         >
                             {cat.logo}
